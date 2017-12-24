3eab7432e3a4944aa23959b8bd8580903d1b1d36
mabangjin <mabangjin@outlook.com>
2017-12-24 16:32:07 +0800

    初始化

---
+++ cmake-build-debug/CMakeFiles/MaEngine.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MaEngine.dir/main.cpp.obj"
+  "MaEngine.pdb"
+  "MaEngine.exe"
+  "MaEngine.exe.manifest"
+  "libMaEngine.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MaEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
